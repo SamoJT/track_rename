cba9516bd027949103b73ebd999d2c16632975cc
SamoJT <samojt@gmail.com>
2021-01-19 21:10:33 +0000

    Improved output of unchanged files.

--- track_rename.py
+++ track_rename.py
@@ -9,30 +9,29 @@ def extract_rename(directory, sl):
     # 'Title' metadata available, and finally if there are any
     # forbidden file name characters. Once checks have been
     # passed the file is renamed using os module.
-    
-    unchanged = []
+
+    unchanged = [[], [], [], [], []]
     for root, dirs, files in walk(directory):
         for name in files:
             full_path = path.join(root, name)
             try:
-                ext = Path(full_path).suffix        
+                ext = Path(full_path).suffix
                 meta_title = File(full_path, easy=True).get('title')
-
                 print(f'Old: {name}')
                 print(f'New: {meta_title[0]}{ext}\n')
                 rename(full_path, f'{root}{sl}{meta_title[0]}{ext}')
-                
+
             except AttributeError:
-                unchanged.append(f"UNSUPPORTED FILE TYPE: {ext} -- {name}")
+                unchanged[0].append(f"UNSUPPORTED FILE TYPE: {ext} -- {name}")
             except TypeError:
-                unchanged.append(f'NO TITLE METADATA: {name}')
+                unchanged[1].append(f'NO TITLE METADATA: {name}')
             except FileExistsError:
-                unchanged.append(f"DUPLICATE FILE EXISTS: {name}")
+                unchanged[2].append(f"DUPLICATE FILE EXISTS: {name}")
             except OSError:
-                unchanged.append(f"RESTRICTED CHARACTERS: {meta_title[0]}")
+                unchanged[3].append(f"RESTRICTED CHARACTERS: {meta_title[0]}")
             except:
-                unchanged.append(f"UNKNOWN ERROR: {name} ")
-    
+                unchanged[4].append(f"UNKNOWN ERROR: {name} ")
+
     return unchanged
 
 def main():
@@ -48,9 +47,10 @@ def main():
     
     if unchanged != None:
         print("The following were not renamed:")
-        for item in unchanged:
-            print(item)
+        for exception in unchanged:
+            for error in exception:
+                print(error)
     return
-               
+
 if __name__ == '__main__':
     main()

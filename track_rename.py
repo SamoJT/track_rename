cf473d732833b77310c307925b9a9360d6d90536
SamoJT <samojt@gmail.com>
2021-01-19 20:42:12 +0000

    Added directory traversal allowing muliple directories files be renamed. Added TypeError exception. Added OS detection to enure correct slash is used in path.

--- track_rename.py
+++ track_rename.py
@@ -1,8 +1,9 @@
 from mutagen import File
-from os import path, rename, scandir
+from os import path, rename, scandir, walk
 from pathlib import Path
+from platform import system
 
-def extract_rename(directory):
+def extract_rename(directory, sl):
     # Takes a full path to a directory containing music files.
     # These are then checked for file type, if there is
     # 'Title' metadata available, and finally if there are any
@@ -10,37 +11,46 @@ def extract_rename(directory):
     # passed the file is renamed using os module.
     
     unchanged = []
-    for file in scandir(directory):       
-        try:
-            ext = Path(file.path).suffix        
-            meta_title = File(file, easy=True).get('title')
-            if meta_title == None:
-                unchanged.append(f'{file.name} due to no "Title" metadata.')
-                continue
+    for root, dirs, files in walk(directory):
+        for name in files:
+            full_path = path.join(root, name)
+            try:
+                ext = Path(full_path).suffix        
+                meta_title = File(full_path, easy=True).get('title')
 
-            print(f'Old: {file.name}')
-            print(f'New: {meta_title[0]}{ext}\n')
-            rename(file.path, f'{directory}{meta_title[0]}{ext}')
-
-        except AttributeError:
-            unchanged.append(f"{file.name} due to unsupported file type.")
-        except FileExistsError:
-            unchanged.append(f"{file.name} due to duplicate file existing.")
-        except OSError:
-            unchanged.append(f"{file.name} due to forbidden characters.")
-        except:
-            unchanged.append(f"{file.name} due to unknown - Likely a Mutagen error.")
+                print(f'Old: {name}')
+                print(f'New: {meta_title[0]}{ext}\n')
+                rename(full_path, f'{root}{sl}{meta_title[0]}{ext}')
+                
+            except AttributeError:
+                unchanged.append(f"UNSUPPORTED FILE TYPE: {ext} -- {name}")
+            except TypeError:
+                unchanged.append(f'NO TITLE METADATA: {name}')
+            except FileExistsError:
+                unchanged.append(f"DUPLICATE FILE EXISTS: {name}")
+            except OSError:
+                unchanged.append(f"RESTRICTED CHARACTERS: {meta_title[0]}")
+            except:
+                unchanged.append(f"UNKNOWN ERROR: {name} ")
     
     return unchanged
 
 def main():
-    directory = ""
-    unchanged = extract_rename(directory)
+    directory = "C:\\Users\\Sam\\Music\\D&B\\"
+    if system() == 'Windows':
+        sl = '\\'
+    elif system() == 'Linux':
+        sl = '/'
+    else:
+        return "Unable to determine OS."
+    
+    unchanged = extract_rename(directory, sl)
     
     if unchanged != None:
         print("The following were not renamed:")
         for item in unchanged:
             print(item)
+    return
                
 if __name__ == '__main__':
     main()
